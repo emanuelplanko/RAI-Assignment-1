94f82dcbe1930b55e9c25c66c2087b37a72917ec
emanuelplanko <planko.emanuel@gmail.com>
2025-03-17 00:22:39 +0100

    implemented delete method, updated list.php and show.php files

--- views/articles/list.php
+++ views/articles/list.php
@@ -8,15 +8,15 @@
         foreach ($articles as $article) {
     ?>
             <div class="article">
+                <br>
                 <h4><?php echo $article->title; ?></h4>
                 <p><?php echo $article->abstract; ?></p>
                 <p>
                     Objavil: <?php echo $article->user->username; ?>,
                     <?php echo date_format(date_create($article->date), 'd. m. Y \ob H:i:s'); ?>
                 </p>
-                <a href="/articles/show?id=<?php echo $article->id; ?>"><button>Preberi več</button></a>
+                <a href="/articles/show?id=<?php echo $article->id; ?>&source=list"><button>Preberi več</button></a>
                 <a href="/articles/edit?id=<?php echo $article->id; ?>"><button>Uredi novico</button></a>
-                <!-- Delete button with a JavaScript confirmation -->
                 <a href="/articles/delete?id=<?php echo $article->id; ?>"
                     onclick="return confirm('Ste prepričani, da želite izbrisati to novico?');">
                     <button>Izbriši novico</button>
--- views/articles/show.php
+++ views/articles/show.php
@@ -1,11 +1,19 @@
 <div class="container">
     <h3>Seznam novic</h3>
     <br>
+
+    <?php
+    $backLink = "/";
+    if (isset($_GET["source"]) && $_GET["source"] === "list") {
+        $backLink = "/articles/list";
+    }
+    ?>
+
     <div class="article">
         <h4><?php echo $article->title; ?></h4>
         <p><b>Povzetek:</b> <?php echo $article->abstract; ?></p>
         <p><?php echo $article->text; ?></p>
         <p>Objavil: <?php echo $article->user->username; ?>, <?php echo date_format(date_create($article->date), 'd. m. Y \ob H:i:s'); ?></p>
-        <a href="/"><button>Nazaj</button></a>
+        <a href="<?php echo $backLink; ?>"><button>Nazaj</button></a>
     </div>
 </div>
